8050a12af58c612aaaa1a390b9d62ab3becfa353
James Luna <jamesluna@Jamess-MacBook-Pro-3.local>
2020-01-11 15:34:45 -0800

    Removed 'Main Psych' class from import, won't be needing it in further files.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
-  PsychMain,
   eRelative,
   eWetBulb,
   eDewPoint
 } from './psych.js'
 
 
-
-
-
-
 function App() {
   return (
-    <div className="App">
+    <div claassName="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
