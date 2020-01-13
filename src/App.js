13f4d762ab1e800b1791adfb73b85daac7dea187
James Luna <jamesluna@Jamess-MacBook-Pro-3.local>
2020-01-12 16:32:18 -0800

    Created static header

--- src/App.js
+++ src/App.js
@@ -9,13 +9,26 @@ import {
 
 function Header() {
   return (
-    <div className='Header'>
-      <div className='headerContent'>
-        <div className='logo'>
-          <img src={require('./Icons/header_logo.svg')} />
+    <div className="Header">
+      <div className="headerContent">
+        <div className="logo">
+          <img src={require("./Icons/header_logo.svg")} />
         </div>
-        <div className='menu'>
-          
+        <div className="menu">
+          <svg
+            width="43"
+            height="31"
+            viewBox="0 0 43 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0 5.16667V0H43V5.16667H0ZM0 18.0833H43V12.9167H0V18.0833ZM0 31H43V25.8333H0V31Z"
+              fill="#F4F4F4"
+            />
+          </svg>
         </div>
       </div>
     </div>
