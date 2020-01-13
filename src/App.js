f53264dec06c75ab13a1ffaf015c930d091023c4
James Luna <jamesluna@Jamess-MacBook-Pro-3.local>
2020-01-12 16:22:31 -0800

    Removed generated template, created primary components - currently working on header portion.

--- src/App.js
+++ src/App.js
@@ -7,24 +7,41 @@ import {
   eDewPoint
 } from './psych.js'
 
+function Header() {
+  return (
+    <div className='Header'>
+      <div className='headerContent'>
+        <div className='logo'>
+          <img src={require('./Icons/header_logo.svg')} />
+        </div>
+        <div className='menu'>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+function Body() {
+  return (
+    <div className='Body'>
+
+    </div>
+  )
+}
+function Footer() {
+  return (
+    <div className='Footer'>
+
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div claassName="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
