7e68b44b55ebbc781dd7ee42234a4ac352475f08
James Luna <jamesaluna1718@gmail.com>
2020-01-19 14:30:15 -0800

    updated side bar - made it horizontal and included a home button

--- src/App.js
+++ src/App.js
@@ -67,10 +67,10 @@ function Header() {
     return function() {
       const navigation = document.querySelector('.sideBar');
       if (toggled) {
-        navigation.style.width = '0px';
+        navigation.style.height = '0px';
         toggled = false;
       } else {
-        navigation.style.width = '175px';
+        navigation.style.height = '100px';
         toggled = true;
       }
     }
@@ -94,7 +94,7 @@ function Header() {
               fill-rule="evenodd"
               clip-rule="evenodd"
               d="M0 5.16667V0H43V5.16667H0ZM0 18.0833H43V12.9167H0V18.0833ZM0 31H43V25.8333H0V31Z"
-              fill="#F4F4F4"
+              fill="#FFFFFF"
             />
           </svg>
         </div>
@@ -105,6 +105,7 @@ function Header() {
 function SideBar() {
   return (
     <div className='sideBar'>
+      <div className='sideBarContent'>
       <div className='sideBarLinksWrapper'>
         <div className='sideBarLink'>
           <span>Guide</span>
@@ -115,6 +116,12 @@ function SideBar() {
         <div className='sideBarLink'>
           <span>Try</span>
         </div>
+      </div>
+      <div class='home-button-wrapper'>
+        <svg width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M4.5 13.2353H0L15 0L30 13.2353H25.5V25H16.5V16.1765H13.5V25H4.5V13.2353ZM22.5 10.5732L15 3.95557L7.5 10.5732V22.0585H10.5V13.235H19.5V22.0585H22.5V10.5732Z" fill="#F0F0F0"/>
+        </svg>
+      </div>
       </div>
     </div>
   )
