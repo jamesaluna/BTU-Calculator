344deb9fef74809b8a64efe793085760aedc0dd9
James Luna <jamesaluna1718@gmail.com>
2020-01-20 18:48:08 -0800

    Created 404 Page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h2>Sorry!</h2>
+            <small>We don't have what you're looking for here...</small>
+        </div>
+    )
+};
+
+export default NotFoundPage;
